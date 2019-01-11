54ba9a15ec332a6dc7c225061b6edb94412c93b2
Romain Moranges <romain.moranges@gmail.com>
2019-01-11 11:46:01 +0100

    Organisation du code

--- exercice2/version4/script.js
+++ exercice2/version4/script.js
@@ -1,58 +1,5 @@
-
-function completeListe(recherche, e) {
-    $get(`ville.php`, {recherche : recherche}, done, error);
-    
-    function done(rep) {
-        let res = JSON.parse(rep.responseText);
-        
-        if (e.keyCode !== 37 || e.keyCode !== 38 || e.keyCode !== 39 || e.keyCode !== 40) {
-            datalist.innerHTML = '';
-            for (let j = 0; j < res.length; j++) {
-                let ville = document.createElement('option');
-                datalist.appendChild(ville);
-                document.getElementsByTagName('option')[j].value = res[j].ville;
-            }
-        } else {
-            // console.log(e.keyCode + " c'est la touche ");
-            return true;
-        }
-        iVille.onchange = () => {
-            console.log(iVille.value);
-            console.log(res.length);
-            for (let k=0; k<res.length; k++) {
-                if(iVille.value === res[k].ville) {
-                    let latitude = res[k].latitude;
-                    let longitude = res[k].longitude;
-                    map.setView([latitude, longitude], 10);
-                    
-                }
-            }
-            
-        }
-    }
-    
-    function error(r) {
-        console.log(r);
-    }
-}
-
 let iVille = document.getElementsByTagName('input')[0];
 let datalist = document.getElementsByTagName('datalist')[0];
-
-iVille.onkeyup = (e) => {
-    let rVille = iVille.value;
-
-    // console.log(rVille);
-    if (rVille.length > 2) {
-        // console.log('on peut lancer la requête');
-        // console.log(rVille);
-        completeListe(rVille, e);
-    } else {
-        // console.log('il manque des caractères avant de lancer la recherche');
-        datalist.innerHTML = '';
-    }
-}
-
 
 /* Leaflet */
 
@@ -64,3 +11,48 @@ let osm = new L.TileLayer(osmUrl, {
 });
 map.setView([48.86, 2.34445], 10);
 map.addLayer(osm);
+
+/* Fonctions */
+function completeListe(recherche, e) {
+
+    $get(`ville.php`, {recherche : recherche}, done, error);
+    
+    function done(rep) {
+        let res = JSON.parse(rep.responseText);
+        
+        if (e.keyCode !== 37 || e.keyCode !== 38 || e.keyCode !== 39 || e.keyCode !== 40) {
+            datalist.innerHTML = '';
+
+            for (let j = 0; j < res.length; j++) {
+                let ville = document.createElement('option');
+                datalist.appendChild(ville);
+                ville.value = res[j].ville;
+
+                if(iVille.value.toUpperCase() == res[j].ville) {
+                    let latitude = res[j].latitude;
+                    let longitude = res[j].longitude;
+                    map.setView([latitude, longitude], 14);
+                }
+            }
+        } else {
+            return true;
+        }
+    }
+    
+    function error(r) {
+        console.log(r);
+    }
+}
+
+/* appel des fonctions */
+
+iVille.onkeyup = (event) => {
+    if (iVille.value.length > 2) {
+        completeListe(iVille.value, event);
+    } else {
+        // console.log('il manque des caractères avant de lancer la recherche');
+        datalist.innerHTML = '';
+    }
+}
+
+
